e79ad5e6c014185e176fa8976c9163accbe3d300
dapeli <terryinzaghi@163.com>
2015-11-01 23:38:28 +0800

    Create dli_modified_GPUImageJFAVoronoiFilter.h

---
+++ dli_modified_GPUImageJFAVoronoiFilter.h
@@ -0,0 +1,22 @@
+#import "GPUImageFilter.h"
+
+@interface GPUImageJFAVoronoiFilter : GPUImageFilter
+{
+    GLuint secondFilterOutputTexture;
+    GLuint secondFilterFramebuffer;
+    
+    
+    GLint sampleStepUniform;
+    GLint sizeUniform;
+    NSUInteger numPasses;
+    
+}
+
+@property (nonatomic, readwrite) CGSize sizeInPixels;
+
+////
+@property (nonatomic, readwrite) CGFloat initStep;
+////
+
+
+@end
